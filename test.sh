19c4f708705a4a21d5307ab943e0b884154d1750
pong3ds <chaiyapong@3dsinteractive.com>
2023-10-03 13:28:58 +0700

    add test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# test_api is the main function to run test
+function test_api() {
+    # If found go in default path, it will use go from default path
+    GO=/usr/local/go/bin/go
+    if [ -f "$GO" ]; then
+        /usr/local/go/bin/go mod init automationworkshop/main
+        /usr/local/go/bin/go mod tidy
+        /usr/local/go/bin/go get
+        /usr/local/go/bin/go mod vendor
+        /usr/local/go/bin/go test .
+    else 
+        go mod init automationworkshop/main
+        go mod tidy
+        go get
+        go mod vendor
+        go test .
+    fi
+}
+
+# Run main test process
+test_api
